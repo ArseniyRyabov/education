6e5a0ec7ba4109153a8acde3e95c54e9766980b2
Arseniy Ryabov <riabov.arsenii@gmail.com>
2025-11-06 11:19:40 +0400

    added controller (#1)

---
+++ src/main/java/com/github/arseniyryabov/education/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.github.arseniyryabov.education.controller;
+
+import com.github.arseniyryabov.education.EducationApplication;
+import org.springframework.boot.SpringApplication;
+
+public class Controller {
+    public static void main(String[] args) {
+        SpringApplication.run(EducationApplication.class, args);
+
+
+}
+}
