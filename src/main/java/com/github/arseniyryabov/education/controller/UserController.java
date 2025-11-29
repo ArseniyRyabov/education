5b74c767e2efd53ad5c3f92e0af48a7621be2ba2
Arseniy Ryabov <riabov.arsenii@gmail.com>
2025-11-29 15:08:45 +0400

    added Exception

--- src/main/java/com/github/arseniyryabov/education/controller/UserController.java
+++ src/main/java/com/github/arseniyryabov/education/controller/UserController.java
@@ -1,31 +1,34 @@
 package com.github.arseniyryabov.education.controller;
 
 import com.github.arseniyryabov.education.controller.model.UserCreatingRequest;
-import com.github.arseniyryabov.education.repository.UserRepository;
+import com.github.arseniyryabov.education.entity.UserEntity;
+import com.github.arseniyryabov.education.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.github.arseniyryabov.education.controller.model.UserResponse;
-import com.github.arseniyryabov.education.service.UsersService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UsersService usersService;
 
-    @GetMapping("/user")
-    public String getAll() {
-        return "Список пользователей";
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse userResponse = usersService.getUserById(id);
+        if (userResponse != null) {
+            return ResponseEntity.ok(userResponse);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @PostMapping("/user")
+    @PostMapping
     public ResponseEntity<Long> create(@RequestBody UserCreatingRequest userCreatingRequest) {
         Long userId = usersService.create(userCreatingRequest);
         return new ResponseEntity<>(userId, HttpStatus.CREATED);
     }
-
-
-
 }
--- src/test/java/com/github/arseniyryabov/education/service/UsersServiceTest.java
+++ src/test/java/com/github/arseniyryabov/education/service/UsersServiceTest.java
@@ -1,7 +1,9 @@
 package com.github.arseniyryabov.education.service;
 
 import com.github.arseniyryabov.education.controller.model.UserCreatingRequest;
+import com.github.arseniyryabov.education.controller.model.UserResponse;
 import com.github.arseniyryabov.education.entity.UserEntity;
+import com.github.arseniyryabov.education.exceptions.UserNotFoundException;
 import com.github.arseniyryabov.education.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -50,4 +54,31 @@ class UsersServiceTest {
 
         assertEquals(1L, userId);
     }
+
+    @Test
+    public void testGetUserById_UserExists() {
+        UserEntity userEntity = new UserEntity("Иванов", "Иван", "Иванович");
+        userEntity.setId(1L);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(userEntity));
+
+        UserResponse userResponse = usersService.getUserById(1L);
+
+        assertNotNull(userResponse);
+        assertEquals("Иван", userResponse.getUserName());
+        assertEquals("Иванов", userResponse.getLastName());
+        assertEquals("Иванович", userResponse.getSecondName());
+    }
+
+    @Test
+    public void testGetUserById_UserNotFound() {
+        when(userRepository.findById(2L)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(UserNotFoundException.class, () -> {
+            usersService.getUserById(2L);
+        });
+
+        assertEquals("Пользователь с ID 2 не найден", exception.getMessage());
+    }
+
+
 }
