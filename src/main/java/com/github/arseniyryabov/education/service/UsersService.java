84c753be04e494259a3af5794f4b1f4105c97cd2
Arseniy Ryabov <riabov.arsenii@gmail.com>
2025-11-29 15:08:45 +0400

    added Exception

--- src/main/java/com/github/arseniyryabov/education/service/UsersService.java
+++ src/main/java/com/github/arseniyryabov/education/service/UsersService.java
@@ -1,5 +1,7 @@
 package com.github.arseniyryabov.education.service;
 
+import com.github.arseniyryabov.education.controller.model.UserResponse;
+import com.github.arseniyryabov.education.exceptions.UserNotFoundException;
 import com.github.arseniyryabov.education.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.
@@ -8,6 +10,8 @@ import com.github.arseniyryabov.education.entity.UserEntity;
 import com.github.arseniyryabov.education.controller.model.UserCreatingRequest;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UsersService {
     @Autowired
@@ -20,5 +24,16 @@ public class UsersService {
         userEntity.setSecondName(request.getSecondName());
         UserEntity savedUser = userRepository.save(userEntity);
         return savedUser.getId();
+    }
+
+    public UserResponse getUserById(Long id) {
+        Optional<UserEntity> userEntityOptional = userRepository.findById(id);
+        if (userEntityOptional.isPresent()) {
+            UserEntity userEntity = userEntityOptional.get();
+            return new UserResponse(userEntity.getId(), userEntity.getUserName(), userEntity.getLastName(), userEntity.getSecondName());
+        }
+        else {
+            throw new UserNotFoundException("Пользователь с ID " + id + " не найден");
+        }
     }
 }
